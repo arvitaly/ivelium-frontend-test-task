d282bf7dee855e125bd7dacb523e56b4a12a39a5
Vitaly <arvitaly@list.ru>
2023-12-26 22:10:38 +0700

    added build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/ivelium-frontend-test-task',
-  build: {
-    outDir: './docs'
-  }
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/",
+    build: {
+      outDir: process.env.VITE_BUILD_PATH,
+    },
+  });
+};
